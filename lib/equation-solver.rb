3746f0afbe64158f8e73bcd66224836128b953ee
Zachary Perlmutter <zrp200@gmail.com>
2014-10-16 16:59:17 -0700

    Update equation-solver.rb

--- lib/equation-solver.rb
+++ lib/equation-solver.rb
@@ -5,10 +5,12 @@ parameter variable must be a String.
 equation("5*x", "10", "x") will return "x = 2"
 =end
 def equation(left, right, variable, set=-1000..1000)
-	varsub = proc {|side, value| side.gsub(/#{variable}/, value).to_i}
+	varsub = proc do |side, value, variable|
+		side.gsub(/#{variable}/, value).to_i if side.include? variable
+	end
 	set = set.to_a
 	for value in set
-		l, r = varsub.call(left, value.to_s), varsub.call(right, value.to_s)
+		l, r = varsub.call(left, value.to_s, variable).to_i, varsub.call(right, value.to_s, variable).to_i
 		return "#{variable} = #{value}" if l == r
 	end	
 	return "#{variable} = nil"
