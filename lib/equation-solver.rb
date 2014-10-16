a4dd70a57310124e165839d7ba6b6696a64e32d5
Zachary Perlmutter <zrp200@gmail.com>
2014-10-16 16:39:54 -0700

    Update equation-solver.rb

--- lib/equation-solver.rb
+++ lib/equation-solver.rb
@@ -1,1 +1,17 @@
-def equation
+=begin
+parameters left and right must be Strings.
+parameter set may be a range or an Array. It can also be left alone.
+parameter variable must be a String.
+equation("5*x", "10", "x") will return "x = 2"
+=end
+def equation(left, right, variable, set=-1000..1000)
+	varsub = proc {|side, value| side.gsub(/#{variable}/, value).to_i}
+	set = set.to_a
+	for value in set
+		l, r = varsub.call(left, value.to_s), varsub.call(right, value.to_s)
+		return "#{variable} = #{value}" if l == r
+	end	
+	return "#{variable} = nil"
+end
+			
+        
