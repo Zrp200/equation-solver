7f2e76606880471407203e348b72b9242f22206a
Zachary Perlmutter <zrp200@gmail.com>
2014-10-17 11:57:10 -0700

    Update equation-solver.rb
    [skip ci]

--- lib/equation-solver.rb
+++ lib/equation-solver.rb
@@ -1,11 +1,14 @@
 =begin
 parameters left and right must be Strings.
 parameter set may be a range or an Array. It can also be left alone.
-parameter variable must be a String.
+parameter variable must be a String or Regexp.
 equation("5*x", "10", "x") will return "x = 2"
 =end
-def equation(left, right, variable, set = -1000..1000)
-	varsub = proc { |side, value, variable| side.gsub(variable, value).to_i }
+def equation(equation, variable, set = -1000..1000)
+	variable = Regexp.new variable
+	equation =~ /=/
+	left, right = $~.pre_match, $~.post_match
+	varsub = proc { |side, value, variable| side.gsub(variable, value) }
 	set = set.to_a
 	for value in set
 		l, r = varsub.call(left, value.to_s, variable).to_i, varsub.call(right, value.to_s, variable).to_i
