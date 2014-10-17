af38accffecb6b3f9d329c67e65b63927e4fd819
Zachary Perlmutter <zrp200@gmail.com>
2014-10-17 12:14:57 -0700

    Update equation-solver.rb

--- lib/equation-solver.rb
+++ lib/equation-solver.rb
@@ -4,9 +4,14 @@ parameter set may be a range or an Array. It can also be left alone.
 parameter variable must be a String or Regexp.
 equation("5*x", "10", "x") will return "x = 2"
 =end
+class Equation
+	def initialize(equation, variable, set=(-100..100))
+		raise StandardError unless variable.match equation
+	end
+end
 def equation(equation, variable, set = -1000..1000)
 	variable = Regexp.new variable
-	equation =~ /=/
+	/=/ =~ equation	
 	left, right = $~.pre_match, $~.post_match
 	varsub = proc { |side, value, variable| side.gsub(variable, value) }
 	set = set.to_a
