b43157f877733b6eca54da5239178b3177d422c0
Zachary Perlmutter <zrp200@gmail.com>
2014-10-17 17:10:24 -0700

    Update equation-solver.rb

--- lib/equation-solver.rb
+++ lib/equation-solver.rb
@@ -9,13 +9,27 @@ class Equation
 		raise StandardError unless variable == equation
 	end
 end
+OPS = /[\+\-\=\*\/]/
+FORMAT = proc do |side|
+	s = side.partition OPS
+	s1, s2, s3 = s[0].to_i, s[1], s[2].to_i
+	side = case s2
+		when "+" then s1 + s3
+		when "-" then s1 - s3
+		when "/" then s1 / s3
+		when "*" then s1 * s3
+	end
+end
 def equation(equation, variable, set = -1000..1000)
 	/\=/ =~ equation	
 	left, right = $~.pre_match, $~.post_match
-	varsub = proc { |side, value, variable| side.gsub(Regexp.new(variable), value) }
+	
+	varsub = proc { |side, value, variable| side.gsub(variable, value) }
 	set = set.to_a
 	for value in set
-		l, r = varsub.call(left, value.to_s, variable).to_i, varsub.call(right, value.to_s, variable).to_i
+		l, r = varsub.call(left, value.to_s, variable), varsub.call(right, value.to_s, variable)
+		FORMAT.call l
+		FORMAT.call r
 		if l == r
 			final = "#{variable.to_s} = #{value}"
 		end
